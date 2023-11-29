9b906f5579dd889d4003b5239954e8a3692a49c9
aishirin <awrnjoomartova@gmail.com>
2023-11-29 22:18:04 +0600

    Tables

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,60 @@
+// pages/index.tsx
+import React from 'react';
 
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <header>
+        <h1>Important tasks!</h1>
+      </header>
 
-function Home() {
-  return(<div>
-    Hello world
-  </div>)
-}
+      <main>
+        <section>
+          <h2>Week-1</h2>
+          <p>Example</p>
+        </section>
 
-export default Home;
+        <section>
+          <h2>Monday</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Challenge</th>
+                <th>Design</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Reading</td>
+                <td>Learning typescript and next</td>
+              </tr>
+              <tr>
+                <td>Blabla</td>
+                <td>Somthing</td>
+              </tr>
+             
+            </tbody>
+          </table>
+        </section>
+
+        <section>
+          <h2>Logic</h2>
+          <div>
+            <p>Example</p>
+          </div>
+          <div>
+            <p>Maybe learning</p>
+          </div>
+        </section>
+      </main>
+
+      <footer>
+        <p>End</p>
+      </footer>
+
+    
+    </div>
+  );
+};
+
+export default HomePage;
