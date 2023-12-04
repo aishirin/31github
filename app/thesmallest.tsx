7e8f3dbf83a499f59d9f683608dbbe187b7d046d
aishirin <awrnjoomartova@gmail.com>
2023-12-04 23:37:08 +0600

    Quadratic function

--- app/thesmallest.tsx
+++ app/thesmallest.tsx
@@ -15,9 +15,19 @@ const MinFunctionPage: React.FC = () => {
   };
 
   const exampleArray = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+  const quadratic = (x1: number, x2: number): number[] => {
+    return [1, -(x2 + x1), x1 * x2];
+  };
+
+  const [resultA, resultB, resultC] = quadratic(2, 3);
+
 
   return (
     <div>
+        <div>
+      <h1>Quadratic Function Example</h1>
+      <p>Result: {resultA}x^2 + {resultB}x + {resultC}</p>
+    </div>
       <h1>Min Function Example</h1>
       <p>Min value: {min(exampleArray, "value")}</p>
       <p>Index of min value: {min(exampleArray, "index")}</p>
