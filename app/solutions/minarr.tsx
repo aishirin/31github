ace249523619bf196bedf0aa46504465852a3007
aishirin <awrnjoomartova@gmail.com>
2023-12-13 21:03:09 +0600

    Test

--- app/solutions/minarr.tsx
+++ app/solutions/minarr.tsx
@@ -1,1 +1,17 @@
+import React from 'react';
 
+
+
+const Startup: React.FC = () => {
+    const iloverin = () => {
+      return 'iloverin';
+    };
+  
+    return (
+      <div>
+        <h1>{iloverin()}</h1>
+      </div>
+    );
+  };
+
+export default Startup;
