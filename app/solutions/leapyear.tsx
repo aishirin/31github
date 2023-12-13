7f44dffdd1792628ddbe3254388b6be617f2a5d6
aishirin <awrnjoomartova@gmail.com>
2023-12-13 21:04:28 +0600

    Typesc remake

--- app/solutions/leapyear.tsx
+++ app/solutions/leapyear.tsx
@@ -1,7 +1,14 @@
-function isLeapYear(y:any) {
-    if (y % 400 == 0) return true
-    if (y % 100 == 0) return false
-    if (y % 4 == 0) return true
-    return false
+// TypeScript version of the function
+
+function isLeapYear(y: number): boolean {
+    if (y % 400 === 0) return true;
+    if (y % 100 === 0) return false;
+    if (y % 4 === 0) return true;
+    return false;
   }
+  
+  // Example usage
+  const year: number = 2024;
+  const isLeap: boolean = isLeapYear(year);
+  console.log(`${year} is a leap year: ${isLeap}`);
   
